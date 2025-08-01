e248322c108d72235aabad261f73fa53c4dfc264
Philip Niron Nithianandan <philip.niron@gmail.com>
2025-08-02 10:19:25 +1200

    MSE800 - Factorial function added
    v-1.0

---
+++ WEEK1/factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+print(factorial(4))
