f11b090afacdfbffd675d5c96d0f173dbd321397
Philip Niron Nithianandan <philip.niron@gmail.com>
2025-08-10 11:31:30 +1200

    MSE800 - Remove __init__
    Removed the __init__ method and updated all methods to accept the text as a parameter instead of using an instance variable. Updated usage examples to reflect these changes, allowing the class to be used without instantiating with text.

--- WEEK2/string_manipulator.py
+++ WEEK2/string_manipulator.py
@@ -1,27 +1,27 @@
 class StringManipulator:
-    def __init__(self, text):
-        self.text = text
+    # __init__ method removed
     
-    def find_character(self, char):
-        return self.text.find(char)
+    def find_character(self, text, char):
+        return text.find(char)
     
-    def find_length(self):
-        return len(self.text)
+    def find_length(self, text):
+        return len(text)
     
-    def convert_to_uppercase(self):
-        return self.text.upper()
+    def convert_to_uppercase(self, text):
+        return text.upper()
 
-# Create an instance of the StringManipulator class
-name = StringManipulator("example")
+# Use the StringManipulator class without __init__
+manipulator = StringManipulator()
+text = "example"
 
-# Call the find_character method on the object
-character = name.find_character('x')
-print("Character position is : ",character)  # Output: 1
+# Call the find_character method
+character = manipulator.find_character(text, 'x')
+print("Character position is : ", character)  # Output: 1
 
 # Call the find_length and convert_to_uppercase methods
-length = name.find_length()
-print("Length of the word is : ",length)  # Output: 7
+length = manipulator.find_length(text)
+print("Length of the word is : ", length)  # Output: 7
 
 # Call the convert_to_uppercase method
-uppercase_text = name.convert_to_uppercase()
-print("Upper case word is : ",uppercase_text)  # Output: EXAMPLE
+uppercase_text = manipulator.convert_to_uppercase(text)
+print("Upper case word is : ", uppercase_text)  # Output: EXAMPLE
