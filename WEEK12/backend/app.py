18f76e48074bc9b6008f1736652e70d4f33c2e47
Philip Niron Nithianandan <philip.niron@gmail.com>
2025-10-18 09:01:27 +1300

    MSE800 - Add new routes for bye, username, and square
    Added three new routes: '/bye' returns a farewell message, '/username/<name>' displays a message with the provided username, and '/square/<int:number>' returns the square of the given number.

--- WEEK12/backend/app.py
+++ WEEK12/backend/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello():
     return '<h1>Hello, Flask!</h1>'
 
+@app.route('/bye')
+def bye():
+    return '<h1>Bye, Flask!</h1>'
+
+@app.route('/username/<name>')
+def learn(name):
+    return f'<h1>{name} is learning Flask!</h1>'
+
+@app.route('/square/<int:number>')
+def calculate_square(number):
+    return f'<h1>The square of {number} is {number**2}!</h1>'
+
 if __name__ == '__main__':
     app.run(debug=True)
